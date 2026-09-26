b6b3edda8e59b45055181da6abbd6a468b66dbe2
faddyy <fady10407@gmail.com>
2026-09-26 17:21:57 +0300

    edit backendurl

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://backend-invoicer.vercel.app/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
